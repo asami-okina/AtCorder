aaeb9aa4451b44ffc2b019a18e8dd95c3141344d
asami <asami.kawasaki.08@gmail.com>
2022-07-06 17:38:46 +0900

    ac

--- abc083b-some-sums/src/main.rs
+++ abc083b-some-sums/src/main.rs
@@ -1,50 +1,30 @@
-use proconio::input;
+use proconio::*;
 use proconio::source::auto::AutoSource;
-use std::collections::HashSet;
 
 fn main() {
-    let source = AutoSource::from(
-        "
-        100 4 16
-        ",
-    );
+    // let source = AutoSource::from(
+    //     "
+    //     100 4 16
+    //     ",
+    // );
 
     input!(
-        from source,
-        sum: String,
+        // from source,
+        sum: i32,
         a: i32,
         b: i32
     );
 
-    let mut numric_each_digits_vec: Vec<i32> = vec![];
-    let mut result_hash_set: HashSet<i32> = HashSet::new();
+    // 結果
+    let mut sum_result: i32 = 0;
 
-    for i in sum.as_str().chars() {
-        let numric_each_digits = i as i32 - 48;
-        numric_each_digits_vec.push(numric_each_digits);
-    }
+    for i in 0..sum + 1 {
+        let sum: i32 = i.to_string().chars().map(|x| x as i32 -48).sum();
 
-    let sum: i32 = sum.parse().unwrap();
-
-    for i in 0..numric_each_digits_vec.len() {
-        for j in 0..numric_each_digits_vec[i] + 1 {
-            for k in 0..10 {
-                // 桁ごとの数字を合算
-                let combined_number_each_digits: i32 = j + k;
-                if a <= combined_number_each_digits && combined_number_each_digits <= b {
-                    let string_result: String = format!("{}{}", j, k);
-                    let num_result: i32 = string_result.parse().unwrap();
-                    if num_result <= sum {
-                        result_hash_set.insert(num_result);
-                    }
-                }
-            }
+        if a <= sum && b >= sum {
+            sum_result += i;
         }
     }
-    let mut result: i32 = 0;
+    println!("{}",sum_result);
 
-    for num in result_hash_set {
-        result += num;
-    }
-    println!("{}", result);
 }
