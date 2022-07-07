310f40065927f4003086754de91e808a504e31b0
asami <asami.kawasaki.08@gmail.com>
2022-07-07 09:15:48 +0900

    ac

--- abc085c-otoshidama/src/main.rs
+++ abc085c-otoshidama/src/main.rs
@@ -4,7 +4,7 @@ use proconio::source::auto::AutoSource;
 fn main() {
     // let source = AutoSource::from(
     //     "
-    //     9 45000
+    //     2000 20000000
     //     "
     // );
 
@@ -14,14 +14,19 @@ fn main() {
         total: i32
     );
 
+    // ten: 10,000円
+    // five: 5,000円
+    // one: 1,000円
+
     for ten in 0..=count {
         // 5000円札は1万円札の枚数を引いた分だけfor分で回す
         for five in 0..=count - ten {
-            if ten * 10000 + five * 5000 + (count - ten - five) * 1000 == total {
-                println!("{} {} {}", ten, five, count - ten - five);
+            let one = count - ten - five;
+            if ten * 10000 + five * 5000 + one * 1000 == total {
+                println!("{} {} {}", ten, five, one);
                 return;
             }
         }
     }
-    println!("{} {} {}", -1, -1 ,-1);
+    println!("{} {} {}", -1, -1, -1);
 }
