3b2cb3ff2c4482c1ec688952acd48c7a46502da3
asami <asami.kawasaki.08@gmail.com>
2022-07-11 16:55:32 +0900

    ac

--- abc236-a/src/main.rs
+++ abc236-a/src/main.rs
@@ -2,15 +2,15 @@ use proconio::input;
 use proconio::source::auto::AutoSource;
  
 fn main() {
-    // let source = AutoSource::from(
-    //     "
-    //     aaaabbbb
-    //     1 8
-    //     "
-    // );
+    let source = AutoSource::from(
+        "
+        aaaabbbb
+        1 8
+        "
+    );
 
     input! {
-        // from source,
+        from source,
         s: String,
         a: usize,
         b: usize,
