9ca3626f30b6f75dc80a30639fab94e80588b066
asami <asami.kawasaki.08@gmail.com>
2022-08-07 13:15:43 +0900

    コメント追加

--- abc263/src/main.rs
+++ abc263/src/main.rs
@@ -3,15 +3,15 @@ use proconio::source::auto::AutoSource;
 use std::collections::HashMap;
 
 fn main() {
-    // let source = AutoSource::from(
-    //     "
-    //     10
-    //     1 2 3 4 5 6 7 8 9         
-    //     ",
-    // );
+    let source = AutoSource::from(
+        "
+        10
+        1 1 2 3
+        ",
+    );
 
     input! {
-        // from source,
+        from source,
         n: usize,
         list: [usize; n - 1]
     };
@@ -20,6 +20,10 @@ fn main() {
     let mut c = list[n-2];
     let mut count = 1;
     loop {
+        // N 4
+        // (親:list)1 1 3の場合もある。
+        // 人2の親が人1,人3の親が人1,人4の親が人3。人2と人3は兄弟。
+        // 兄弟がいる場合もあるから、兄弟の場合は世代カウントしたくない。
         if c == 1 {
             break;
         }
